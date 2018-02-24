b214778e85b18d05e45dc9bcfe4a3d38cacfae94
Tony Roberts <123ananan@gmail.com>
2018-02-24 00:41:48 -0500

    initial
    initial

---
+++ question.js
@@ -0,0 +1,90 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'What is the worst case time complexity of insertion in a Singly-Linked List?': [
+                'O N',
+                'O 1',
+                'O n log n',
+                'O n squared',
+            ],
+        },
+        {
+            'What is the Queue deletion time?': [
+                'O 1',
+                'O N',
+                'O n log n',
+                'O n squared',
+            ],
+        },
+		        {
+            'What is the Array deletion time?': [
+                'O N',
+                'O 1',
+                'O n log n',
+                'O n squared',
+            ],
+        },
+		        {
+            'What is the worst case time complexity of sorting with Bubble Sort?': [
+                'O n squared',
+                'O N',
+                'O n log n',
+                'O 1',
+            ],
+        },
+		        {
+            'Which of these sorting algorithms would you use if space complexity was your main concern?': [
+                'Insertion Sort',
+                'Merge sort',
+                'Quick sort',
+                'Tree sort',
+            ],
+        },
+		        {
+            'Which of these data structures would you use if your only priority was to access a given element quickly?': [
+                'Array',
+                'Stack',
+                'Queue',
+                'Singly-Linked List',
+            ],
+        },
+		        {
+            'Which of these algorithms has a non-constant space complexity?': [
+                'Quick Sort',
+                'Heap Sort',
+                'Selection Sort',
+                'Bubble Sort',
+            ],
+        },
+		        {
+            'What is the Queue deletion time?': [
+                'O 1',
+                'O N',
+                'O n log n',
+                'O n squared',
+            ],
+        },
+		        {
+            'What is the worst case time complexity of deleting a node from a linked list if you do not have a reference to the node?': [
+                'O n log n',
+                'O N over 2',
+                '0 n',
+                'O n squared',
+            ],
+        },
+        {
+            'What is the time complexity of deleting a node from a linked list, given a reference to it?': [
+                'O 1',
+                'O N over 2',
+                'O n log n',
+                'O n',
+            ],
+        },
+    ],
+};
